1c4e3eb50388467b0db13c67f320f4fb7db4e3eb
AliHashemi2000 <111985802+AliHashemi2000@users.noreply.github.com>
2022-11-24 21:44:31 +0000

    Updated

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-let loginForm=document.querySelector('.login-form');
+let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.add('active');
+   loginForm.classList.add('active');
 }
 
-
 document.querySelector('#close-login-form').onclick = () =>{
-    loginForm.classList.remove('active');
+   loginForm.classList.remove('active');
 }
 
 let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .nav'); 
+let navbar = document.querySelector('.header .nav');
 
 menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active'); 
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
 }
 
 window.onscroll = () =>{
-    loginForm.loginForm.classList.remove('active');
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active'); 
+   loginForm.classList.remove('active');
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
 
-
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-    }else{
-        document.querySelector('.heder').classList.remove('active');
-    }
+   if(window.scrollY > 0){
+      document.querySelector('.header').classList.add('active');
+   }else{
+      document.querySelector('.header').classList.remove('active');
+   }
 }
 
